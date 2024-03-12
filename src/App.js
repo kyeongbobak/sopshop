3515ff10716bfe08de61610343f198c80bc62aad
kyeongbobak <kyeong0914@gmail.com>
2024-03-12 17:21:35 +0900

    design : 구매회원 로그인 화면 구현, 로그인 탭 클릭 시 활성 탭 상태 변경과 스타일 업데이트 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import BuyerLogin from "./pages/BuyerLogin/BuyerLogin";
 import GlobalStyle from "./style/GlobalStyle";
 
 function App() {
   return (
     <>
-      <GlobalStyle>
-        <h1>hello</h1>
-      </GlobalStyle>
+      <GlobalStyle />
+      <BuyerLogin />
     </>
   );
 }
--- src/pages/BuyerLogin/BuyerLogin.jsx
+++ src/pages/BuyerLogin/BuyerLogin.jsx
@@ -1,8 +1,51 @@
+import LogoImage from "../../assets/Logo-hodu.png";
+import { useState } from "react";
+import { LoginWrapper, FormBox, SiteLogo, LoginForm, Label, StyledInput, LoginButton, LinkContainer, StyledLink, UserTypeTabs, LoginTab, SellerLoginTabLink, BuyerLoginTabLink } from "./BuyerLoginStyle";
+
 export default function BuyerLogin() {
+  const [activeTab, setActiveTab] = useState("buyer");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <>
-      <h1>hello</h1>
-      <h2>hello2</h2>
+      <LoginWrapper>
+        <SiteLogo>
+          <img src={LogoImage} alt="" />
+        </SiteLogo>
+        <FormBox>
+          <UserTypeTabs>
+            <LoginTab>
+              <BuyerLoginTabLink onClick={() => handleTabClick("buyer")} isActive={activeTab === "buyer"}>
+                구매회원 로그인
+              </BuyerLoginTabLink>
+            </LoginTab>
+            <LoginTab>
+              <SellerLoginTabLink onClick={() => handleTabClick("seller")} isActive={activeTab === "seller"}>
+                판매회원 로그인
+              </SellerLoginTabLink>
+            </LoginTab>
+          </UserTypeTabs>
+          <LoginForm>
+            <Label className="a11y-hidden" htmlFor="UserId">
+              아이디
+            </Label>
+            <StyledInput type="id" placeholder="아이디" />
+            <Label className="a11y-hidden" htmlFor="UserPassword">
+              비밀번호
+            </Label>
+            <StyledInput type="password" placeholder="비밀번호" />
+            <LoginButton>로그인</LoginButton>
+          </LoginForm>
+        </FormBox>
+        <LinkContainer>
+          <StyledLink>회원가입</StyledLink>
+          <span>|</span>
+          <StyledLink>비밀번호 찾기</StyledLink>
+        </LinkContainer>
+      </LoginWrapper>
     </>
   );
 }
