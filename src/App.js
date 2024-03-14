27e2f55a04d2db849175b3292b038d423741fab7
kyeongbobak <kyeong0914@gmail.com>
2024-03-14 16:29:31 +0900

    design : 구매회원 메인페이지 footer UI 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import GlobalStyle from "./style/GlobalStyle";
 import AuthContextProvider from "./contexts/AuthContext";
+import BuyerMainPage from "./pages/BuyerMainPage";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
-      <AuthContextProvider></AuthContextProvider>
-    </>
+      <AuthContextProvider>
+        <BuyerMainPage />
+      </AuthContextProvider>
+    </Router>
   );
 }
 export default App;
---
+++ src/pages/BuyerMainPage.jsx
@@ -0,0 +1,62 @@
+import { Footer, FooterWrapper, FooterMenuWrap, FooterLinkMenu, FooterLink, StyledLink, StyledBoldLink, FooterTop, FooterBottom, FooterSnsWrap, FooterSns, FooterSnsLink } from "./BuyerMainPageStyle";
+import InstagramIcon from "../assets/icon-insta.png";
+import FaceBookIcon from "../assets/icon-fb.png";
+import YouTuBeIcon from "../assets/icon-yt.png";
+
+export default function BuyerMainPage() {
+  return (
+    <Footer>
+      <FooterWrapper>
+        <FooterTop>
+          <FooterMenuWrap>
+            <FooterLinkMenu>
+              <FooterLink>
+                <StyledLink to="">SopShop 소개</StyledLink>
+              </FooterLink>
+              <FooterLink>
+                <StyledLink to="">이용약관</StyledLink>
+              </FooterLink>
+              <FooterLink>
+                <StyledBoldLink to="">개인정보처리방침</StyledBoldLink>
+              </FooterLink>
+              <FooterLink>
+                <StyledLink to="">전자금융거래약관</StyledLink>
+              </FooterLink>
+              <FooterLink>
+                <StyledLink to="">청소년보호정책</StyledLink>
+              </FooterLink>
+              <FooterLink>
+                <StyledLink to="">제휴문의</StyledLink>
+              </FooterLink>
+            </FooterLinkMenu>
+          </FooterMenuWrap>
+          <FooterSnsWrap>
+            <FooterSns>
+              <FooterSnsLink>
+                <StyledLink to="">
+                  <img src={InstagramIcon} alt="" />
+                </StyledLink>
+              </FooterSnsLink>
+              <FooterSnsLink>
+                <StyledLink to="">
+                  <img src={FaceBookIcon} alt="" />
+                </StyledLink>
+              </FooterSnsLink>
+              <FooterSnsLink>
+                <StyledLink to="">
+                  <img src={YouTuBeIcon} alt="" />
+                </StyledLink>
+              </FooterSnsLink>
+            </FooterSns>
+          </FooterSnsWrap>
+        </FooterTop>
+        <FooterBottom>
+          <span className="first-span">(주)SopShop</span>
+          <span>서울시 중구 신당동 마린 빌딩 2층</span>
+          <span>사업자번호 123-1234-5678 </span>
+          <span>대표: 박태리</span>
+        </FooterBottom>
+      </FooterWrapper>
+    </Footer>
+  );
+}
