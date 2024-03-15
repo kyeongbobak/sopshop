d2bf1046e5611f86aba7028d3e37c853a508d018
kyeongbobak <kyeong0914@gmail.com>
2024-03-15 15:12:50 +0900

    refactor : BuyerMainPage에서 Footer 부분 컴포넌트 따로 분리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import GlobalStyle from "./style/GlobalStyle";
 import AuthContextProvider from "./contexts/AuthContext";
+import BuyerFooter from "./components/BuyerFooter/BuyerFooter";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <AuthContextProvider></AuthContextProvider>
+      <Router>
+        <GlobalStyle />
+        <AuthContextProvider>
+          <BuyerFooter />
+        </AuthContextProvider>
+      </Router>
     </>
   );
 }
