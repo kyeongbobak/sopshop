f091c73089f414e6176be3a2127e724f1fcc1424
kyeongbobak <kyeong0914@gmail.com>
2024-03-18 14:10:29 +0900

    fix : app.js에 Router 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import GlobalStyle from "./style/GlobalStyle";
 import { AuthProvider } from "./contexts/AuthContext";
+import BuyerMainPage from "./pages/BuyerMainPage/BuyerMainPage";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
-      <AuthProvider></AuthProvider>
-    </>
+      <AuthProvider>
+        <BuyerMainPage />
+      </AuthProvider>
+    </Router>
   );
 }
 export default App;
