52b4b3326f36299491feed2583bb6c6fd5fe48c9
kyeongbobak <kyeong0914@gmail.com>
2024-03-25 10:07:52 +0900

    fix : 상품리스트 변경

--- src/pages/BuyerMainPage/BuyerMainPage.jsx
+++ src/pages/BuyerMainPage/BuyerMainPage.jsx
@@ -29,7 +29,8 @@ export default function BuyerMainPage() {
         const data = await Promise.all(res.map((res) => (token ? res.data : res.json())));
         const mergedData = [...data[0].results, ...data[1].results];
         console.log(data);
-        setProducts([...mergedData.slice(0, 3), ...mergedData.slice(-4, -2)]);
+        setProducts([...mergedData.slice(1, 4), ...mergedData.slice(-3, -1)]);
+        console.log(...mergedData);
       } catch (error) {
         console.log("error");
       }
