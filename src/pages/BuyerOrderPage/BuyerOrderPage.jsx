4dd21684afaed176c3c3c0929c098c9113796b3c
kyeongbobak <kyeong0914@gmail.com>
2024-04-16 15:00:39 +0900

    design : 주문 내역 페이지 UI 구현, CSS 스타일 추가

--- src/pages/BuyerOrderPage/BuyerOrderPage.jsx
+++ src/pages/BuyerOrderPage/BuyerOrderPage.jsx
@@ -1,3 +1,28 @@
+import { useContext, useEffect, useState } from "react";
+import ProductInfoHeader from "../../components/ProductInfoHeader/ProductInfoHeader";
+import BuyerHeader from "../../components/BuyerHeader/BuyerHeader";
+import { OrderPageWrapper, OrderPageTitle, OrderPageContents, OrderItemWrapper, OrderItemImage, OrderItemDiscount, OrderItemDelivery, OrderItemPrice } from "./BuyerOrderPageStyle";
+import { AuthContext } from "../../contexts/AuthContext";
+import axios from "axios";
+
 export default function BuyerOrderPage() {
-  return <></>;
+  return (
+    <>
+      <BuyerHeader />
+      <OrderPageWrapper>
+        <OrderPageTitle>Order</OrderPageTitle>
+        <ProductInfoHeader type="order"></ProductInfoHeader>
+        <OrderPageContents>
+          {orderList.map((list, i) => (
+            <OrderItemWrapper>
+              <OrderItemImage></OrderItemImage>
+              <OrderItemDiscount></OrderItemDiscount>
+              <OrderItemDelivery></OrderItemDelivery>
+              <OrderItemPrice></OrderItemPrice>
+            </OrderItemWrapper>
+          ))}
+        </OrderPageContents>
+      </OrderPageWrapper>
+    </>
+  );
 }
---
+++ src/pages/BuyerOrderPage/BuyerOrderPageStyle.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const OrderPageWrapper = styled.div`
+  width: 1280px;
+  margin: 0 auto;
+`;
+
+export const OrderPageTitle = styled.h1`
+  text-align: center;
+  font-size: var(--font-extra-size);
+  margin: 54px 0 52px;
+`;
+
+export const OrderPageContents = styled.div``;
+
+export const OrderItemWrapper = styled.div``;
+
+export const OrderItemImage = styled.div``;
+
+export const OrderItemDiscount = styled.div``;
+
+export const OrderItemDelivery = styled.div``;
+
+export const OrderItemPrice = styled.div``;
