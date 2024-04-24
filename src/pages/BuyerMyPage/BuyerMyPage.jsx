939683903b1337e88cec2ec8a6214e783b4e5208
kyeongbobak <kyeong0914@gmail.com>
2024-04-24 11:22:07 +0900

    feat : 마이페이지 레이아웃과 CSS 스타일 추가

--- src/pages/BuyerMyPage/BuyerMyPage.jsx
+++ src/pages/BuyerMyPage/BuyerMyPage.jsx
@@ -1,12 +1,44 @@
 import BuyerHeader from "../../components/BuyerHeader/BuyerHeader";
+import Button from "../../components/Button/Button";
 import axios from "axios";
 import { AuthContext } from "../../contexts/AuthContext";
-
+import {
+  BuyerMyPageWrapper,
+  BuyerMyPageTitle,
+  BuyerMyPageHeader,
+  BuyerMyPageContents,
+  OrderRecordsWrapper,
+  ProductImage,
+  ProductInfo,
+  ProductPrice,
+  ProductQauntity,
+  PointsEarned,
+  ShippingOption,
+  ShippingFee,
+  OrderRecordsImage,
+  ProductTotalPrice,
+} from "./BuyerMyPageStyle";
 import { useContext, useEffect, useState } from "react";
 
 export default function BuyerMyPage() {
   const { token, isLoggedIn } = useContext(AuthContext);
-  const [orderHistory, setOrderHistory] = useState("");
+  const [orderProductInfo, setOrderProductInfo] = useState("");
+
+  console.log(orderProductInfo);
+
+  const getOrderProductInfo = async () => {
+    try {
+      const instance = axios.create({
+        headers: {
+          Authorization: `JWT ${token}`,
+        },
+      });
+
+      // const res = await instance.get(`https://openmarket.weniv.co.kr/products/${}/`)
+    } catch (error) {
+      console.log("error");
+    }
+  };
 
   const getOrderHistory = async () => {
     try {
@@ -17,7 +49,7 @@ export default function BuyerMyPage() {
       });
       const res = await instance.get(`https://openmarket.weniv.co.kr/order/`);
       const data = await res.data;
-      console.log(data);
+      console.log(data.results);
     } catch (error) {
       console.log(error);
     }
@@ -30,6 +62,37 @@ export default function BuyerMyPage() {
   return (
     <>
       <BuyerHeader></BuyerHeader>
+      <BuyerMyPageWrapper>
+        <BuyerMyPageTitle>Order Records</BuyerMyPageTitle>
+        <BuyerMyPageHeader>
+          <li>이미지</li>
+          <li>상품정보</li>
+          <li>판매가</li>
+          <li>수량</li>
+          <li>적립금</li>
+          <li>배송구분</li>
+          <li>배송비</li>
+          <li>합계</li>
+        </BuyerMyPageHeader>
+        <BuyerMyPageContents>
+          <OrderRecordsWrapper>
+            <ProductImage>
+              <img src="" alt="" />
+            </ProductImage>
+            <ProductInfo></ProductInfo>
+            <ProductPrice></ProductPrice>
+            <ProductQauntity></ProductQauntity>
+            <PointsEarned></PointsEarned>
+            <ShippingOption></ShippingOption>
+            <ShippingFee></ShippingFee>
+            <OrderRecordsImage>
+              <img src="" alt="" />
+            </OrderRecordsImage>
+            <ProductTotalPrice></ProductTotalPrice>
+          </OrderRecordsWrapper>
+        </BuyerMyPageContents>
+        <Button MsButton>Prev</Button>
+      </BuyerMyPageWrapper>
     </>
   );
 }
---
+++ src/pages/BuyerMyPage/BuyerMyPageStyle.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const BuyerMyPageWrapper = styled.div`
+  width: 1280px;
+  margin: 0px auto;
+`;
+
+export const BuyerMyPageTitle = styled.div`
+  font-size: var(--font-extra-size);
+  margin: 54px 0 52px;
+  text-align: center;
+`;
+
+export const BuyerMyPageHeader = styled.ul`
+  display: flex;
+  font-size: var(--font-lg-size);
+  border-top: 1px solid var(--black-color);
+  border-bottom: 1px solid var(--black-color);
+  padding: 20px 0;
+  text-align: center;
+
+  li {
+    width: 160px;
+  }
+
+  li:nth-child(2) {
+    width: 240px;
+  }
+
+  li:nth-child(4) {
+    width: 80px;
+  }
+`;
+
+export const BuyerMyPageContents = styled.div``;
+
+export const OrderRecordsWrapper = styled.div``;
+
+export const ProductImage = styled.div``;
+
+export const ProductInfo = styled.div``;
+
+export const ProductPrice = styled.p``;
+
+export const ProductQauntity = styled.p``;
+
+export const PointsEarned = styled.p``;
+
+export const ShippingOption = styled.div``;
+
+export const ShippingFee = styled.p``;
+
+export const OrderRecordsImage = styled.div``;
+
+export const ProductTotalPrice = styled.p``;
